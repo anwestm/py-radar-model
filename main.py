c2b08f49debcfd15f6dfba8b13254fa05ea7f214
Andreas Westman <anwestm@kth.se>
2024-10-27 11:27:50 +0100

    Add chirping

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from mpmath import linspace
+#from mpmath import linspace
 
 #import signal_generation.nco
 import signal_generation.pulse_generator
 import matplotlib.pyplot as plt
 import numpy as np
+import signal_generation.pulse_memory
 
 
 def main():
@@ -12,23 +13,31 @@ def main():
 
     Fs = 1e5
     ptg = signal_generation.pulse_generator.PulseGenerator(Fs)
+    pm = signal_generation.pulse_memory.PulseMemory()
 
     x = []
     amplitude = 2
     freq = 8000
 
-    ptg.start_pulse_train(8000, amplitude, 0.01, 20)
-    for i in range(50):
-        x.append(ptg.sample())
+    ptg.start_chirped_pulse_train(freq, amplitude, 0.01, 20, 2000)
+    for i in range(10000):
+        sample = ptg.sample()
+        x.append(sample)
+        pm.handle_sample(sample, ptg.inside_pulse_window())
         ptg.tick()
 
 
-    fig = plt.figure(figsize=(10, 14))
-    axs = fig.subplot_mosaic([["Signal", "Signal"], ["Re{Signal}", "Im{Signal}"], ["Phase", "Magnitude Log"]])
+    fig = plt.figure(figsize=(10, 12))
+    axs = fig.subplot_mosaic([["Signal", "Latest Pulse"], ["Re{Signal}", "Im{Signal}"], ["Phase", "Magnitude Log"]])
     axs["Signal"].set_title("Signal")
     axs["Signal"].set_xlabel("Time (s)")
     axs["Signal"].set_ylabel("Amplitude")
     axs["Signal"].plot(np.arange(0, len(x) / Fs, 1 / Fs), x, color="C0")
+
+    axs["Latest Pulse"].set_title("Latest Pulse")
+    axs["Latest Pulse"].set_xlabel("Time (s)")
+    axs["Latest Pulse"].set_ylabel("Amplitude")
+    axs["Latest Pulse"].plot(np.arange(0, len(pm.get_latest_pulse()) / Fs, 1 / Fs), pm.get_latest_pulse(), color="C0")
 
     axs["Re{Signal}"].set_title("Re{Signal}")
     axs["Re{Signal}"].set_xlabel("Time (s)")
